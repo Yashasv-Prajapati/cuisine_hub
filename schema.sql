c83c599f0d17ddef400c6f98b3a9d8b9e9d77cfe
Yashasv-Prajapati <yamuprajapati05@gmail.com>
2023-11-27 22:10:47 +0530

    some changes

--- schema.sql
+++ schema.sql
@@ -26,12 +26,8 @@ create table raw_material(
 	id int auto_increment primary key,
     name varchar(50),
     price int,
-    transaction_time timestamp,
     exp_time timestamp,
-    quantity_left int check(quantity_left >= 0),
-    user_id int,
-    status varchar(10) check( status in ("approved","pending")),
-    foreign key (user_id) references user(id)
+    quantity_left int check(quantity_left >= 0)
 );
 
 -- recipe information 
@@ -58,13 +54,15 @@ create table sells(
 	user_id int not null,
     raw_material_id int not null,
     transaction_time timestamp,
+    units int,
+    status varchar(10) check( status in ("approved","pending")),
     foreign key(user_id) references user(id),
     foreign key(raw_material_id) references raw_material(id),
     primary key(user_id, raw_material_id, transaction_time)
 );
 
 -- relationship between recipes and raw_material
-create table is_made_of(
+create table ingredient(
 	recipe_id int not null,
     raw_material_id int not null,
     quantity_required int check(quantity_required > 0), -- quantity of this raw_material required to make one unit of the recipe
